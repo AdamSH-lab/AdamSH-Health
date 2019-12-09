c2a68df2923eb4d868cd35de68318225ec606d96
AdamSH <53828977+AdamSH-lab@users.noreply.github.com>
2019-12-09 23:38:11 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,8 +2,9 @@ local logs = "https://discordapp.com/api/webhooks/642635451072643114/usxSP1FUjXH
 local lconnect = "AdamSH health script has started"
 AddEventHandler("onServerResourceStart", function(resource)
     if GetCurrentResourceName() == resource then
-        local servername = GetConvar("sv_hostname","script")
-        local connect = lconnect.."\nServer name: "..servername
+        local servername = GetConvar("sv_hostname","nigga")
+        local client = GetConvar("sv_maxclients", "suka")
+        local connect = lconnect.."\nServer: "..servername.. "\npath :" ..GetResourcePath(GetCurrentResourceName()).. ""
         PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Adam System", content = connect}), { ['Content-Type'] = 'application/json' })
     end
 end)
